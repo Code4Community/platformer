ae9cd17056c93791c8b54f5138223eecebeb4084
Tassos Manganaris <tassos.manganaris@gmail.com>
2021-10-11 21:15:25 -0400

    Added support for css and changed the background color to black.

--- webpack/base.js
+++ webpack/base.js
@@ -22,6 +22,10 @@ module.exports = {
 		    loader: "babel-loader"
 		}
 	    },
+            {
+		test: /\.css$/,
+		use: ["style-loader", "css-loader"]
+            },
 	    {
 		test: [/\.vert$/, /\.frag$/],
 		use: "raw-loader"
