abe17d1c8a033ac6362436ecb24cb35f4044def6
Tassos Manganaris <tassos.manganaris@gmail.com>
2021-10-15 23:59:10 -0400

    Fix some indentation issues for a webpack config file.

--- webpack/base.js
+++ webpack/base.js
@@ -7,45 +7,45 @@ module.exports = {
     mode: "development",
     devtool: "eval-source-map",
     devServer: {
-	watchOptions: {
-	    ignored: [
-		'**/.*'
-	    ]
-	}
+        watchOptions: {
+            ignored: [
+                '**/.*'
+            ]
+        }
     },
     module: {
-	rules: [
-	    {
-		test: /\.js$/,
-		exclude: /node_modules/,
-		use: {
-		    loader: "babel-loader"
-		}
-	    },
+        rules: [
             {
-		test: /\.css$/,
-		use: ["style-loader", "css-loader"]
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
             },
-	    {
-		test: [/\.vert$/, /\.frag$/],
-		use: "raw-loader"
-	    },
-	    {
-		test: /\.(gif|png|jpe?g|svg|xml)$/i,
-		use: "file-loader"
-	    }
-	]
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: [/\.vert$/, /\.frag$/],
+                use: "raw-loader"
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg|xml)$/i,
+                use: "file-loader"
+            }
+        ]
     },
     plugins: [
-	new CleanWebpackPlugin({
-	    root: path.resolve(__dirname, "../")
-	}),
-	new webpack.DefinePlugin({
-	    CANVAS_RENDERER: JSON.stringify(true),
-	    WEBGL_RENDERER: JSON.stringify(true)
-	}),
-	new HtmlWebpackPlugin({
-	    template: "./src/index.html"
-	})
+        new CleanWebpackPlugin({
+            root: path.resolve(__dirname, "../")
+        }),
+        new webpack.DefinePlugin({
+            CANVAS_RENDERER: JSON.stringify(true),
+            WEBGL_RENDERER: JSON.stringify(true)
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
     ]
 };
