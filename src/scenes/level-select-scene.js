2011ff8b60a5651651faa7d637849a9bde056016
Tassos Manganaris <tassos.manganaris@gmail.com>
2022-04-11 20:19:44 -0400

    Formatted and improved levels 5 and 6.

--- src/scenes/level-select-scene.js
+++ src/scenes/level-select-scene.js
@@ -2,7 +2,7 @@ import Phaser from "phaser";
 
 import skyImage from "../assets/sky.png";
 import levelMap from "../levels.js";
-import {levelsDefeated} from "./game-scene.js";
+import { levelsDefeated } from "./game-scene.js";
 export default class LevelSelectScene extends Phaser.Scene {
   constructor() {
     super("LevelSelect");
@@ -19,24 +19,24 @@ export default class LevelSelectScene extends Phaser.Scene {
     /*  Manage auth to select levels only after defeating previous level */
     let currentLevel = 0;
     Object.keys(levelMap).forEach((levelKey) => {
-          if(currentLevel <= levelsDefeated.size) {
-              const levelText = this.add.text(0, 0, levelKey, {
-                fontSize: "32px",
-                fill: "#fff",
-              });
-              levelText.setInteractive();
-              levelText.on("pointerdown", () => {
-                this.scene.start(levelKey);
-              });
-              levelButtonGroup.add(levelText);
-          } else {
-              const levelText = this.add.text(0, 0, levelKey, {
-                fontSize: "32px",
-                fill: "#aaa",
-              });
-              levelButtonGroup.add(levelText);
-          }
-          currentLevel+=1;
+      if (currentLevel <= levelsDefeated.size) {
+        const levelText = this.add.text(0, 0, levelKey, {
+          fontSize: "32px",
+          fill: "#fff",
+        });
+        levelText.setInteractive();
+        levelText.on("pointerdown", () => {
+          this.scene.start(levelKey);
+        });
+        levelButtonGroup.add(levelText);
+      } else {
+        const levelText = this.add.text(0, 0, levelKey, {
+          fontSize: "32px",
+          fill: "#aaa",
+        });
+        levelButtonGroup.add(levelText);
+      }
+      currentLevel += 1;
     });
     /*
     Object.keys(levelMap).forEach((levelKey) => {
